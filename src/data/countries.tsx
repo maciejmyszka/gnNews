bfed5a3f810d42cfdab0e3c94fff4821226287f0
Maciej Myszka <maciejmyszka99@gmail.com>
2023-03-21 22:41:26 +0100

    Added basic main view, countries data

---
+++ src/data/countries.tsx
@@ -0,0 +1,50 @@
+export const countries = [
+  {
+    id: 0,
+    name: 'Poland',
+    symbol: 'pl',
+    to: '/country/poland',
+  },
+  {
+    id: 1,
+    name: 'France',
+    symbol: 'fr',
+    to: '/country/france',
+  },
+  {
+    id: 2,
+    name: 'Portugal',
+    symbol: 'pt',
+    to: '/country/portugal',
+  },
+  {
+    id: 3,
+    name: 'Italy',
+    symbol: 'it',
+    to: '/country/italy',
+  },
+  {
+    id: 4,
+    name: 'Turkey',
+    symbol: 'tr',
+    to: '/country/turkey',
+  },
+  {
+    id: 5,
+    name: 'Germany',
+    symbol: 'de',
+    to: '/country/germany',
+  },
+  {
+    id: 6,
+    name: 'USA',
+    symbol: 'us',
+    to: '/country/usa',
+  },
+  {
+    id: 7,
+    name: 'Austria',
+    symbol: 'at',
+    to: '/country/austria',
+  },
+];
--- src/views/MainView/index.tsx
+++ src/views/MainView/index.tsx
@@ -1,33 +1,139 @@
-import { Button, Layout, Space, Typography } from 'antd';
-import { Content, Footer, Header } from 'antd/es/layout/layout';
-import Sider from 'antd/es/layout/Sider';
-import { AppstoreOutlined, UnorderedListOutlined } from '@ant-design/icons';
+import {
+  Button,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  IconButton,
+  Image,
+  ListItem,
+  Text,
+  UnorderedList,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { HamburgerIcon } from '@chakra-ui/icons';
+import { MobileMenu } from '../../layouts/components/MobileMenu';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { articlesState, getArticles } from '../../slices/articlesSlice';
+import { AppDispatch } from '../../config/store';
+import { countries } from '../../data/countries';
+import { ListIcon } from '../../icons/ListIcon';
+import { TilesIcon } from '../../icons/TilesIcon';
 
 export const MainView = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const dispatch = useDispatch<AppDispatch>();
+  const { articles, count } = useSelector(articlesState);
+  const { name } = useParams();
+
+  const [isList, setIsList] = useState<boolean>(true);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const symbol =
+      countries.find(
+        ({ name: countryName }) => countryName.toLowerCase() === name
+      )?.symbol || 'pl';
+
+    dispatch(getArticles(symbol));
+  }, [name]);
+
   return (
-    <Space
-      direction='vertical'
-      style={{ width: '100%', height: '100%' }}
-      size={[0, 48]}
+    <Flex
+      width='100vw'
+      height='100vh'
+      flexDirection='column'
+      position='relative'
     >
-      <Header style={{ display: 'flex' }}>
-        <Typography.Title style={{ color: '#fff' }}>gnNews</Typography.Title>
-        <UnorderedListOutlined />
-        <AppstoreOutlined />
+      <Flex
+        h='6%'
+        w='100%'
+        bg='royalblue'
+        alignItems='center'
+        px='5%'
+        gap='1rem'
+      >
+        <HamburgerIcon
+          style={{ fontSize: '25px', cursor: 'pointer' }}
+          onClick={onOpen}
+        />
+        <Heading onClick={() => navigate('/')}>gnNews</Heading>
+
+        <IconButton aria-label='list' onClick={() => setIsList(true)}>
+          <ListIcon />
+        </IconButton>
+
+        <IconButton aria-label='list' onClick={() => setIsList(false)}>
+          <TilesIcon />
+        </IconButton>
+
         <Button>Open pop-up</Button>
-      </Header>
+      </Flex>
 
-      <Layout>
-        <Sider>
-          <h1>Menu</h1>
-        </Sider>
+      <MobileMenu isOpen={isOpen} onClose={onClose} />
 
-        <Content>
-          <h1>Content</h1>
-        </Content>
-      </Layout>
+      <Flex
+        w='100%'
+        height='94%'
+        flexDirection='column'
+        px='5%'
+        py='1rem'
+        gap='1rem'
+        overflow='scroll'
+      >
+        {isList ? (
+          <UnorderedList>
+            {articles.map(({ publishedAt, title, url, urlToImage }: any) => (
+              <ListItem key={url} flexDirection='column'>
+                <Text>{title}</Text>
 
-      <Footer />
-    </Space>
+                <Flex justifyContent='flex-end' mb='0.5rem'>
+                  <Text>{new Date(publishedAt).toLocaleString()}</Text>
+                </Flex>
+              </ListItem>
+            ))}
+          </UnorderedList>
+        ) : (
+          <Grid
+            templateColumns={[
+              'repeat(1, 1fr)',
+              'repeat(2, 1fr)',
+              'repeat(3, 1fr)',
+              'repeat(4, 1fr)',
+              'repeat(5, 1fr)',
+              'repeat(5, 1fr)',
+            ]}
+            gap={6}
+          >
+            {articles.map(({ publishedAt, title, url, urlToImage }: any) => (
+              <GridItem
+                key={url}
+                flexDirection='column'
+                display='flex'
+                border='1px solid black'
+                borderRadius='0.5rem'
+                p='0.5rem'
+                bg='gray.200'
+              >
+                <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
+                <Text mt='0.5rem'>{title}</Text>
+
+                <Flex justifyContent='flex-end' mb='0.5rem'>
+                  <Text>{new Date(publishedAt).toLocaleString()}</Text>
+                </Flex>
+              </GridItem>
+            ))}
+          </Grid>
+        )}
+
+        <Flex flexDirection='column'>
+          <Text>{new Date().toLocaleString()}</Text>
+          <Text>{count}</Text>
+        </Flex>
+      </Flex>
+    </Flex>
   );
 };
