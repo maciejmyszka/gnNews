c3b2de3aa296d6ea457b0566636df6279f852a3d
Maciej Myszka <maciejmyszka99@gmail.com>
2023-03-29 18:26:00 +0200

    Added theme

---
+++ src/config/theme.ts
@@ -0,0 +1,14 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+    main: '#FF6900',
+    dark: '#020100ed',
+    tumbleweed: '#f26700cc',
+    lightGray: '#080504e3',
+    lightOrange: '#f26700cc',
+    white: '#fff',
+    light: '#D4D4D4',
+    lightButton: '',
+  },
+});
